98adde25ea8ca934f6782a9304c9f73a0c954480
miperi <40628887+miperi@users.noreply.github.com>
2023-01-08 10:21:35 +0200

    sun / 4 to be near to therm on the graph

--- main.ts
+++ main.ts
@@ -1,5 +1,8 @@
 input.onButtonPressed(Button.AB, function () {
     datalogger.deleteLog(datalogger.DeleteType.Full)
+    basic.showIcon(IconNames.No)
+    basic.pause(1000)
+    basic.clearScreen()
 })
 let sunis = 0
 let thermis = 0
@@ -12,13 +15,13 @@ datalogger.setColumnTitles(
 while (0 == 0) {
     basic.clearScreen()
     basic.pause(60000)
-    if (thermis != input.temperature() || sunis != input.lightLevel()) {
+    if (thermis != input.temperature() || sunis != Math.trunc(input.lightLevel() / 4)) {
         datalogger.log(
-        datalogger.createCV("Sun", input.lightLevel()),
+        datalogger.createCV("Sun", Math.trunc(input.lightLevel() / 4)),
         datalogger.createCV("therm", input.temperature())
         )
         thermis = input.temperature()
-        sunis = input.lightLevel()
+        sunis = Math.trunc(input.lightLevel() / 4)
         basic.showLeds(`
             . . . . .
             . . . . .
